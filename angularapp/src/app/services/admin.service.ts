c77d5c17cde28311613dcc1cd490ac0a760d3645
Iamneo <techadmin@iamneo.ai>
2023-11-20 17:04:54 +0000

    Backup Commit

---
+++ angularapp/src/app/services/admin.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ITeam } from '../model/iteam';
+import { IPlayer } from '../model/iplayer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor() { }
+}
+
+
+import { Injectable } from '@angular/core';
+
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamServiceService {
+ 
+  constructor(private httpclient : HttpClient) { }
+ 
+  private url = 'https://8080-addabcfedcbbadfacbbecabcdadeafbbdcaeafe.premiumproject.examly.io/Admin'
+ 
+  getTeams() : Observable<ITeam[]> {
+    return this.httpclient.get<ITeam[]>(this.url + '/ListTeam')
+  }
+ 
+  getTeam(id : number) : Observable<ITeam> {
+    return this.httpclient.get<ITeam>(this.url + '/ListTeam/' + id)
+  }
+ 
+  getPlayer(id : number) : Observable<IPlayer[]> {
+    return this.httpclient.get<IPlayer[]>(this.url + '/DisplayTeamPlayers/(id)?id=' + id)
+  }
+ 
+ 
+ 
+}
