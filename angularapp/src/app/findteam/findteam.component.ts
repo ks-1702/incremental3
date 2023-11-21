078a4de35affb4ff91cd38aee8f435708a565126
Iamneo <techadmin@iamneo.ai>
2023-11-21 17:46:47 +0000

    Backup Commit

--- angularapp/src/app/findteam/findteam.component.ts
+++ angularapp/src/app/findteam/findteam.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../services/admin.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Team } from '../models/team';
 
 @Component({
   selector: 'app-findteam',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindteamComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private as:AdminService,private ar:ActivatedRoute,private router:Router) { }
+teamdata:Team
+id:number
   ngOnInit(): void {
+
+    const tid=this.ar.snapshot.paramMap.get('id')
+    this.id=Number(tid)
+    this.as.findTeam(this.id).subscribe((data:Team) =>{
+      this.teamdata=data;
+    })
+  }
+
+  Show()
+  {
+    this.router.navigate(['/ListTeams'])
   }
 
 }
