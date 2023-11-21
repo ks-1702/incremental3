dd2e3048809648febc5ef1241e0cea567088fdb4
Iamneo <techadmin@iamneo.ai>
2023-11-21 07:12:13 +0000

    Backup Commit

--- angularapp/src/app/addplayers/addplayers.component.ts
+++ angularapp/src/app/addplayers/addplayers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from '../models/player';
 import { AdminService } from '../services/admin.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-addplayers',
@@ -11,7 +12,17 @@ export class AddplayersComponent implements OnInit {
   playerdata:Player={id:0,name:'',age:0,category:'',biddingPrice:0,teamid:0}
 
   constructor(private as:AdminService,private route:Router) { }
-
+  saveData(player:Player):void
+  {
+    this.playerdata=player
+    this.as.addPlayers(this.playerdata).subscribe
+    (
+      ()=>{
+        alert('Record Added Successfully')
+        this.route.navigate(['/GetPlayer']) //redirect back to list-players
+      }
+    )
+  }
   ngOnInit(): void {
   }
 
