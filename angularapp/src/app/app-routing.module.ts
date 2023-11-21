42957ecb6c75324aa1c83e9d5134942bd623738e
Iamneo <techadmin@iamneo.ai>
2023-11-21 08:27:57 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetplayersComponent } from './getplayers/getplayers.component';
+import { AddplayersComponent } from './addplayers/addplayers.component';
 
 
 const routes: Routes = [
+  {path:'GetPlayer',component:GetplayersComponent},
+  {path:'addplayers',component:AddplayersComponent}
   
 ];
 
